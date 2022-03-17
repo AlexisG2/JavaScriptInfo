10eb8d9f5d93b75c96c7dbeaeb30bee91b310bc1
Your name here <GordilloAlexisCTE@gmail.com>
2022-03-16 20:59:08 -0400

    This is my solution

---
+++ exponentiation.js
@@ -0,0 +1,4 @@
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+
+function toThePower(x,n){
+   return Math.pow(x,n);}
