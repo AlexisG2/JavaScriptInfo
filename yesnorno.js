35c8bc2e53595b59a49f20a26818f5d699436765
Your name here <GordilloAlexisCTE@gmail.com>
2022-09-02 22:56:46 -0400

    Daily push

---
+++ yesnorno.js
@@ -0,0 +1,6 @@
+//create a game that terminates at the passage of yes or no
+
+function validate(){
+    let word = "";
+    while(word !== 'yes' && word !== 'no'){
+        word = prompt("Enter any word other than yes or no, say yes or no to end the game.")}}
